bb9b136918cc2bbf4dce08bdd5056030930b9404
007shohanur <shohanur007rm@gmail.com>
2025-08-01 21:41:17 +0600

    last update

--- src/pages/Dashboard/Admin/AdminSidebar.tsx
+++ src/pages/Dashboard/Admin/AdminSidebar.tsx
@@ -20,9 +20,9 @@ export const Sidebar = ({
   };
 
   return (
-    <aside className="lg:w-72 w-full bg-gray-900 text-white flex lg:flex-col flex-row items-start justify-start lg:min-h-screen lg:border-r border-b border-gray-700 shadow-lg">
+    <aside className="lg:w-72 w-full bg-black text-white flex lg:flex-col flex-row items-start justify-start lg:min-h-screen lg:border-r border-b border-gray-700 shadow-lg">
       {/* Header */}
-      <div className="hidden lg:block text-2xl font-bold p-6 text-center border-b border-gray-700 bg-gray-800 lg:w-full">
+      <div className="hidden lg:block text-2xl font-bold p-6 text-center border-b border-gray-700 bg-purple-800 lg:w-full">
         Admin Panel
       </div>
 
